11a313dd2f63bc5f872c23c8898c80813ac7fc50
Jakob Heuser <jakob@felocity.com>
2015-02-11 14:17:06 -0800

    Apache 2.0 license requirements

--- build/test.js
+++ build/test.js
@@ -5,7 +5,7 @@ var mocha = require("gulp-mocha");
 module.exports = function(gulp, depends) {
   gulp.task("test", depends, function() {
     return gulp.src([
-      "test/test_*.js"
+      "test/**/test_*.js"
       ], {read: false})
         .pipe(mocha({reporter: "spec"}));
   });
