61f4dc0659ab1f45d177241f621fa3870b85e4f5
Chris Eppstein <chris@eppsteins.net>
2015-04-20 12:32:21 -0700

    test(sync): Introduce file i/o for real async behavior in testcase.

--- test/test_sync.js
+++ test/test_sync.js
@@ -8,9 +8,11 @@ describe("synchronous rendering", function () {
  it("compiles basic file synchronously", function (done) {
    var eyeglassVersion = JSON.parse(fs.readFileSync(path.resolve(__dirname, "../package.json"))).version;
    var options = {
-     data: "/* Eyeglass version is #{eyeglass-version()} */"
+     root: testutils.fixtureDirectory("function_modules"),
+     data: "/* Eyeglass version is #{eyeglass-version()} */ .test { a: read('a'); }"
    };
-   var expectedOutput = "/* Eyeglass version is \"" + eyeglassVersion + "\" */\n";
+   var expectedOutput = "/* Eyeglass version is \"" + eyeglassVersion + "\" */\n" +
+                        ".test {\n  a: 1; }\n";
    testutils.assertCompilesSync(options, expectedOutput, done);
    done();
  });
