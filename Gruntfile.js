c961064f53f131625de5927513cddd54e1429b64
Chris Eppstein <chris@eppsteins.net>
2015-04-23 13:25:11 -0700

    chore(build): use grunt-release

---
+++ Gruntfile.js
@@ -0,0 +1,12 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    release: {
+      options: {
+      }
+    }
+  });
+  grunt.loadNpmTasks('grunt-release');
+
+  grunt.registerTask('default', []);
+};
