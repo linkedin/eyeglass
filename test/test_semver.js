58250b1dd560ba7d5dc95b2daeade0d3dd66ba34
Jakob Heuser <jakob@felocity.com>
2015-05-20 10:24:43 -0700

    semver support at runtime for eyeglass modules This change adds semver support to eyeglass modules, avoiding the problems typically associated with peerDependencies. Instead, an eyeglass module can add to their package.json:
    ```
    {
      "eyeglass": {
        "engine": "SEMVER_VALUE"
      }
    }
    ```
    
    And set `SEMVER_VALUE` to a semver compatible string. When eyeglass wraps the
    sass options and eyeglass modules are loaded, the `eyeglass.engine` values are
    checked against the currently running eyeglass version. If an incompatible
    engine is found, the user has the option to stop the node-sass process from
    continuing.
    
    To force a hard stop, set `eyeglassStrictVersions: true` in your node-sass
    options.
    
    To silence all messages, set `eyeglassStrictVersions: false` in your node-sass
    options.
    
    By default, warnings are thrown instead of halting, and the value of
    `eyeglassStrictVersions` is `"warn"`.

---
+++ test/test_semver.js
@@ -0,0 +1,46 @@
+"use strict";
+
+var assert = require("assert");
+var testutils = require("./testutils");
+
+var eyeglass = require("../lib").decorate;
+
+describe("semver checking", function () {
+  it("will not let a module through with an engine violation", function (done) {
+    var options = {
+      root: testutils.fixtureDirectory("bad_engine"),
+      data: "#hello { greeting: hello(Chris); }"
+    };
+
+    testutils.assertStderr(function(check) {
+      eyeglass(options);
+      check();
+    }, function(output) {
+      var errored = (output.indexOf("incompatible with eyeglass") >= 0);
+      assert.ok(errored, "Error was not logged to console");
+      done();
+    });
+  });
+
+  it("will throw if eyeglassEngineStrict is set", function (done) {
+    var options = {
+      root: testutils.fixtureDirectory("bad_engine"),
+      data: "#hello { greeting: hello(Chris); }",
+      eyeglassStrictVersions: true
+    };
+
+    testutils.assertStderr(function(check) {
+      var ex;
+      try {
+        eyeglass(options);
+      } catch(e) {
+        ex = e;
+      }
+      assert.ok(ex, "Exception thrown that stops eyeglass processing");
+      check();
+    }, function(output) {
+      // no output check required
+      done();
+    });
+  });
+});
