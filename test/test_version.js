45f972e56a559e26814f29df08b190a82be7d445
Jakob Heuser <jakob@pinterest.com>
2015-08-26 17:01:44 -0700

    enough new test coverage to get istanbul back to passing

--- test/test_version.js
+++ test/test_version.js
@@ -23,4 +23,13 @@ describe("sass version function", function () {
    var expectedOutput = '.version {\n  module-a: "1.0.1"; }\n';
    testutils.assertCompiles(options, expectedOutput, done);
  });
+
+ it("should handle asking for the version of an unknown module", function (done) {
+   var options = {
+     root: testutils.fixtureDirectory("basic_modules"),
+     data: ".ok { color: blue} .version {\nmodule-a: eyeglass-version('not_real_module')}"
+   };
+   var expectedOutput = ".ok {\n  color: blue; }\n";
+   testutils.assertCompiles(options, expectedOutput, done);
+ });
 });
