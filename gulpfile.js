756b6aecd2ad0122798ee5be66555426d760638e
Eugene ONeill <oneill.eugene@gmail.com>
2015-10-19 13:23:35 -0700

    bunch of cleanup so far

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,7 @@ var gulp = require("gulp");
 // Know the node-sass we wrap. It's vital to debugging, tests, and coverage reports
 console.log(require("node-sass").info);
 
-require("./build/lint")(gulp, []);
+require("./build/lint")(gulp);
 require("./build/test")(gulp, ["lint"]);
-require("./build/coverage")(gulp, []);
 
 gulp.task("default", ["test"]);
