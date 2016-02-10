1ec144d38be61c8a67bbc11023874d6ade8702ec
Eugene ONeill <oneill.eugene@gmail.com>
2016-02-09 21:58:12 -0800

    some more fixes for windows paths

--- test/test_assets.js
+++ test/test_assets.js
@@ -277,7 +277,8 @@ describe("assets", function () {
                "@include asset-register(module-a, 'foo/bar.png', 'images/foo/bar.png', " +
                "$uri: 'assets/foo/bar.png');" +
                ".test { foo: inspect($eg-registered-assets); }";
-    var expected = '.test {\n  foo: (module-a: ("foo/bar.png": (filepath: "images/foo/bar.png", ' +
+    var expected = '.test {\n  foo: (module-a: ("foo/bar.png": (' +
+                   'filepath: "' + path.join("images", "foo", "bar.png") + '", ' +
                    'uri: "assets/foo/bar.png"))); }\n';
     var rootDir = testutils.fixtureDirectory("app_assets");
     var eg = new Eyeglass({
