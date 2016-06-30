linkedin/eyeglass

commit b07c3ac20e2a4d82498dc18d06a8424d3e55d2cb

Upgrade broccoli-caching-writer
* reformatting to work with pure JS inheritance now used
* automatically take non-array tree inputs and wrap them (to ensure compatibility)
* in tests, use promises rather then mixing callbacks and promises (removal of done usage)
* use mocha’s promise support directly rather then custom RSVP.on(‘error’ hook.
* rename differs to assertEqualDirs
* assertEqualDirs is always sync, no need to have a callback
* remove forgotten debugger

lib/broccoli_sass_compiler.js
@@ -155,172 +155,172 @@ function forbidNodeSassOption(options, property) {
 //     fullCssFilename: /* Fully expanded and resolved css filename */,
 //     options: /* The options used for this compile */
 //   };
-var BroccoliSassCompiler = CachingWriter.extend({
-  init: function(inputTrees, options) {
-    this._super(inputTrees);
-    this.sass = sass;
-    this.options = copyObject(options || {});
-    this.events = new EventEmitter();
+//
+function BroccoliSassCompiler(inputTrees, options) {
+  CachingWriter.call(this, inputTrees, options);
 
-    moveOption(this.options, this, "cssDir", "sassDir",
-                                   "optionsGenerator", "fullException",
-                                   "verbose", "renderSync",
-                                   "discover", "sourceFiles");
-    if (!this.cssDir) {
-      throw new Error("Expected cssDir option.");
-    }
-    if (!this.optionsGenerator) {
-      this.optionsGenerator = defaultOptionsGenerator;
-    }
-    if (!this.sourceFiles) {
-      this.sourceFiles = [];
-      if (this.discover === false) {
-        throw new Error("sourceFiles are required when discovery is disabled.");
-      } else {
-        // Default to discovery mode if no sourcefiles are provided.
-        this.discover = true;
-      }
-    }
-    forbidNodeSassOption(this.options, "file");
-    forbidNodeSassOption(this.options, "data");
-    forbidNodeSassOption(this.options, "outFile");
+  this.sass = sass;
+  this.options = copyObject(options || {});
+  this.events = new EventEmitter();
 
-    if (this.verbose) {
-      this.events.on("compiled", this.logCompilationSuccess.bind(this));
-      this.events.on("failed", this.logCompilationFailure.bind(this));
-    }
-  },
-
-  logCompilationSuccess: function(details, result) {
-    var timeInSeconds = result.stats.duration / 1000.0;
-    if (timeInSeconds === 0) {
-      timeInSeconds = "0.001"; // nothing takes zero seconds.
-    }
-    var action = colors.inverse.green("compile (" + timeInSeconds + "s)");
-    var message = details.fullSassFilename + " => " + details.cssFilename;
-    console.log(action + " " + message);
-  },
-
-  logCompilationFailure: function(details, error) {
-    var sassFilename = details.sassFilename;
-    var action = colors.bgRed.white("error");
-    var message = colors.red(error.message);
-    var location = "Line " + error.line + ", Column " + error.column;
-    if (error.file.substring(error.file.length - sassFilename.length) !== sassFilename) {
-      location = location + " of " + error.file;
-    }
-    console.log(action + " " + sassFilename + " (" + location + "): " + message);
-  },
-
-  compileTree: function(srcPath, files, destDir) {
-    var self = this;
-    var result = files.reduce(function(promises, sassFile) {
-      return promises.concat(self.compileSassFile(srcPath, sassFile, destDir));
-    }, []);
-    return result;
-  },
-
-  compileSassFile: function(srcPath, sassFilename, destDir) {
-    var self = this;
-    var sassOptions = copyObject(this.options);
-    sassOptions.file = path.join(srcPath, sassFilename);
-    var parsedName = parsePath(sassFilename);
-    if (this.sassDir && parsedName.dir.slice(0, this.sassDir.length) === this.sassDir) {
-      parsedName.dir = parsedName.dir.slice(this.sassDir.length + 1);
-    }
-    parsedName.ext = ".css";
-    parsedName.base = parsedName.name + ".css";
-    var cssFileName = path.join(this.cssDir, formatPath(parsedName));
-    var promises = [];
-    this.optionsGenerator(
-      sassFilename, cssFileName, sassOptions,
-      function(resolvedCssFileName, resolvedOptions) {
-        var details = {
-          srcPath: srcPath,
-          sassFilename: sassFilename,
-          fullSassFilename: resolvedOptions.file,
-          destDir: destDir,
-          cssFilename: resolvedCssFileName,
-          fullCssFilename: path.join(destDir, resolvedCssFileName),
-          options: copyObject(resolvedOptions)
-        };
-        details.options.outFile = details.cssFilename;
-        promises.push(self.compileCssFile(details));
-      });
-    return promises;
-  },
-
-  renderer: function() {
-   if (this.renderSync) {
-     return renderSassSync;
-   } else {
-     return renderSass;
-   }
-  },
-
-  compileCssFile: function(details) {
-    var sass = this.renderer();
-    var success = this.handleSuccess.bind(this, details);
-    var failure = this.handleFailure.bind(this, details);
-
-    return this.events.emit("compiling", details)
-      .then(function() {
-       return sass(details.options)
-         .then(success, failure);
-      });
-
-  },
-
-  handleSuccess: function(details, result) {
-    mkdirp.sync(path.dirname(details.fullCssFilename));
-    fs.writeFileSync(details.fullCssFilename, result.css);
-    return this.events.emit("compiled", details, result);
-  },
-
-  handleFailure: function(details, error) {
-    var failed = this.events.emit("failed", details, error);
-    var rethrow = failed.finally(function() {
-      var message = error.message;
-      var location = "    at " + error.file + ":" + error.line + ":" + error.column;
-      // TODO: implement fullException
-      throw new Error(message + "\n" + location);
-    });
-    return rethrow;
-  },
-
-  filesInTree: function(srcPath) {
-    var sassDir = this.sassDir || "";
-    var files = [];
-    if (this.discover) {
-      var pattern = path.join(srcPath, sassDir, "**", "[^_]*.scss");
-      files = glob.sync(pattern);
-    }
-    this.sourceFiles.forEach(function(sourceFile) {
-      var pattern = path.join(srcPath, sassDir, sourceFile);
-      files = files.concat(glob.sync(pattern));
-    });
-    return unique(files);
-  },
-
-  updateCache: function(srcPaths, destDir) {
-    // TODO: add srcPaths (incl sassDir for the first) to the includePaths option
-    var self = this;
-    var files = {};
-
-    for (var i = 0; i < srcPaths.length; i++) {
-      // TODO: handle indented syntax files.
-      files[srcPaths[i]] = removePathPrefix(srcPaths[i], this.filesInTree(srcPaths[i]));
-    }
-
-    // TODO: limit concurrency?
-    var promises = srcPaths.reduce(function (results, srcPath) {
-      return results.concat(self.compileTree(srcPath, files[srcPath], destDir));
-    }, []);
-
-
-    return RSVP.all(promises);
+  moveOption(this.options, this, "cssDir", "sassDir",
+    "optionsGenerator", "fullException",
+    "verbose", "renderSync",
+    "discover", "sourceFiles");
+  if (!this.cssDir) {
+    throw new Error("Expected cssDir option.");
   }
-});
+  if (!this.optionsGenerator) {
+    this.optionsGenerator = defaultOptionsGenerator;
+  }
+  if (!this.sourceFiles) {
+    this.sourceFiles = [];
+    if (this.discover === false) {
+      throw new Error("sourceFiles are required when discovery is disabled.");
+    } else {
+      // Default to discovery mode if no sourcefiles are provided.
+      this.discover = true;
+    }
+  }
+  forbidNodeSassOption(this.options, "file");
+  forbidNodeSassOption(this.options, "data");
+  forbidNodeSassOption(this.options, "outFile");
+
+  if (this.verbose) {
+    this.events.on("compiled", this.logCompilationSuccess.bind(this));
+    this.events.on("failed", this.logCompilationFailure.bind(this));
+  }
+}
+BroccoliSassCompiler.prototype = Object.create(CachingWriter.prototype);
+BroccoliSassCompiler.prototype.logCompilationSuccess = function(details, result) {
+  var timeInSeconds = result.stats.duration / 1000.0;
+  if (timeInSeconds === 0) {
+    timeInSeconds = "0.001"; // nothing takes zero seconds.
+  }
+  var action = colors.inverse.green("compile (" + timeInSeconds + "s)");
+  var message = details.fullSassFilename + " => " + details.cssFilename;
+  console.log(action + " " + message);
+};
+
+BroccoliSassCompiler.prototype.logCompilationFailure = function(details, error) {
+  var sassFilename = details.sassFilename;
+  var action = colors.bgRed.white("error");
+  var message = colors.red(error.message);
+  var location = "Line " + error.line + ", Column " + error.column;
+  if (error.file.substring(error.file.length - sassFilename.length) !== sassFilename) {
+    location = location + " of " + error.file;
+  }
+  console.log(action + " " + sassFilename + " (" + location + "): " + message);
+};
+
+BroccoliSassCompiler.prototype.compileTree = function(srcPath, files, destDir) {
+  var self = this;
+  var result = files.reduce(function(promises, sassFile) {
+    return promises.concat(self.compileSassFile(srcPath, sassFile, destDir));
+  }, []);
+  return result;
+};
+
+BroccoliSassCompiler.prototype.compileSassFile = function(srcPath, sassFilename, destDir) {
+  var self = this;
+  var sassOptions = copyObject(this.options);
+  sassOptions.file = path.join(srcPath, sassFilename);
+  var parsedName = parsePath(sassFilename);
+  if (this.sassDir && parsedName.dir.slice(0, this.sassDir.length) === this.sassDir) {
+    parsedName.dir = parsedName.dir.slice(this.sassDir.length + 1);
+  }
+  parsedName.ext = ".css";
+  parsedName.base = parsedName.name + ".css";
+  var cssFileName = path.join(this.cssDir, formatPath(parsedName));
+  var promises = [];
+  this.optionsGenerator(
+    sassFilename, cssFileName, sassOptions,
+    function(resolvedCssFileName, resolvedOptions) {
+      var details = {
+        srcPath: srcPath,
+        sassFilename: sassFilename,
+        fullSassFilename: resolvedOptions.file,
+        destDir: destDir,
+        cssFilename: resolvedCssFileName,
+        fullCssFilename: path.join(destDir, resolvedCssFileName),
+        options: copyObject(resolvedOptions)
+      };
+      details.options.outFile = details.cssFilename;
+      promises.push(self.compileCssFile(details));
+    });
+  return promises;
+};
+
+BroccoliSassCompiler.prototype.renderer = function() {
+  if (this.renderSync) {
+    return renderSassSync;
+  } else {
+    return renderSass;
+  }
+};
+
+BroccoliSassCompiler.prototype.compileCssFile = function(details) {
+  var sass = this.renderer();
+  var success = this.handleSuccess.bind(this, details);
+  var failure = this.handleFailure.bind(this, details);
+
+  return this.events.emit("compiling", details)
+    .then(function() {
+      return sass(details.options)
+        .then(success, failure);
+    });
+};
+
+BroccoliSassCompiler.prototype.handleSuccess = function(details, result) {
+  mkdirp.sync(path.dirname(details.fullCssFilename));
+  fs.writeFileSync(details.fullCssFilename, result.css);
+  return this.events.emit("compiled", details, result);
+};
+
+BroccoliSassCompiler.prototype.handleFailure = function(details, error) {
+  var failed = this.events.emit("failed", details, error);
+  var rethrow = failed.finally(function() {
+    var message = error.message;
+    var location = "    at " + error.file + ":" + error.line + ":" + error.column;
+    // TODO: implement fullException
+    throw new Error(message + "\n" + location);
+  });
+  return rethrow;
+};
+
+BroccoliSassCompiler.prototype.filesInTree = function(srcPath) {
+  var sassDir = this.sassDir || "";
+  var files = [];
+  if (this.discover) {
+    var pattern = path.join(srcPath, sassDir, "**", "[^_]*.scss");
+    files = glob.sync(pattern);
+  }
+  this.sourceFiles.forEach(function(sourceFile) {
+    var pattern = path.join(srcPath, sassDir, sourceFile);
+    files = files.concat(glob.sync(pattern));
+  });
+  return unique(files);
+};
+
+BroccoliSassCompiler.prototype.build = function() {
+  var inputPaths = this.inputPaths;
+  var outputPath = this.outputPath;
+
+  // TODO: add inputPaths (incl sassDir for the first) to the includePaths option
+  var self = this;
+  var files = {};
+
+  for (var i = 0; i < inputPaths.length; i++) {
+    // TODO: handle indented syntax files.
+    files[inputPaths[i]] = removePathPrefix(inputPaths[i], this.filesInTree(inputPaths[i]));
+  }
+
+  // TODO: limit concurrency?
+  var promises = inputPaths.reduce(function (results, srcPath) {
+    return results.concat(self.compileTree(srcPath, files[srcPath], outputPath));
+  }, []);
+
+  return RSVP.all(promises);
+};
 
 module.exports = BroccoliSassCompiler;
-

lib/index.js
@@ -19,69 +19,68 @@ function httpJoin() {
   return result;
 }
 
-var EyeglassCompiler = BroccoliSassCompiler.extend({
-  init: function(inputTrees, options) {
-    if (options.configureEyeglass) {
-      this.configureEyeglass = options.configureEyeglass;
-      delete options.configureEyeglass;
-    }
-
-    this.relativeAssets = options.relativeAssets;
-    delete options.relativeAssets;
-
-    if (options.assets) {
-      this.assetDirectories = options.assets;
-      if (typeof this.assetDirectories === "string") {
-        this.assetDirectories = [this.assetDirectories];
-      }
-      delete options.assets;
-    }
-    if (options.assetsHttpPrefix) {
-      this.assetsHttpPrefix = options.assetsHttpPrefix;
-      delete options.assetsHttpPrefix;
-    }
-    this._super(inputTrees, options);
-    this.events.on("compiling", this.handleNewFile.bind(this));
-  },
-
-  handleNewFile: function(details) {
-    if (!details.options.eyeglass) {
-      details.options.eyeglass = {};
-    }
-    if ((this.assetsHttpPrefix || this.assetsRelativeTo) && !details.options.eyeglass.assets) {
-      details.options.eyeglass.assets = {};
-    }
-    if (this.assetsHttpPrefix) {
-      details.options.eyeglass.assets.httpPrefix = this.assetsHttpPrefix;
-    }
-
-    if (this.relativeAssets) {
-      details.options.eyeglass.assets.relativeTo =
-        httpJoin(details.options.eyeglass.httpRoot || "/", path.dirname(details.cssFilename));
-    }
-
-    details.options.eyeglass.buildDir = details.destDir;
-
-    var eyeglass = new Eyeglass(details.options, this._super.sass);
-    if (this.assetDirectories) {
-      for (var i = 0; i < this.assetDirectories.length; i++) {
-        eyeglass.assets.addSource(path.resolve(".", this.assetDirectories[i]), {
-          globOpts: {
-            ignore: ["**/*.js", "**/*.s[ac]ss"]
-          }
-        });
-      }
-    }
-    if (this.configureEyeglass) {
-      this.configureEyeglass(eyeglass, this._super.sass, details);
-    }
-    details.options = eyeglass.options;
-  },
-
-  // Ugh. This method needs to be decomposed into smaller parts.
-  updateCache: function(srcPaths, destDir) {
-    return this._super(srcPaths, destDir);
+function EyeglassCompiler(inputTrees, options) {
+  if (!Array.isArray(inputTrees)) {
+    inputTrees = [inputTrees];
   }
-});
+  if (options.configureEyeglass) {
+    this.configureEyeglass = options.configureEyeglass;
+    delete options.configureEyeglass;
+  }
+
+  this.relativeAssets = options.relativeAssets;
+  delete options.relativeAssets;
+
+  if (options.assets) {
+    this.assetDirectories = options.assets;
+    if (typeof this.assetDirectories === "string") {
+      this.assetDirectories = [this.assetDirectories];
+    }
+    delete options.assets;
+  }
+  if (options.assetsHttpPrefix) {
+    this.assetsHttpPrefix = options.assetsHttpPrefix;
+    delete options.assetsHttpPrefix;
+  }
+
+  // TODO: this should not be accessed before super (ES6 Aligment);
+  BroccoliSassCompiler.call(this, inputTrees, options);
+  this.events.on("compiling", this.handleNewFile.bind(this));
+}
+
+EyeglassCompiler.prototype = Object.create(BroccoliSassCompiler.prototype);
+EyeglassCompiler.prototype.handleNewFile = function(details) {
+  if (!details.options.eyeglass) {
+    details.options.eyeglass = {};
+  }
+  if ((this.assetsHttpPrefix || this.assetsRelativeTo) && !details.options.eyeglass.assets) {
+    details.options.eyeglass.assets = {};
+  }
+  if (this.assetsHttpPrefix) {
+    details.options.eyeglass.assets.httpPrefix = this.assetsHttpPrefix;
+  }
+
+  if (this.relativeAssets) {
+    details.options.eyeglass.assets.relativeTo =
+      httpJoin(details.options.eyeglass.httpRoot || "/", path.dirname(details.cssFilename));
+  }
+
+  details.options.eyeglass.buildDir = details.destDir;
+
+  var eyeglass = new Eyeglass(details.options, this.sass);
+  if (this.assetDirectories) {
+    for (var i = 0; i < this.assetDirectories.length; i++) {
+      eyeglass.assets.addSource(path.resolve(".", this.assetDirectories[i]), {
+        globOpts: {
+          ignore: ["**/*.js", "**/*.s[ac]ss"]
+        }
+      });
+    }
+  }
+  if (this.configureEyeglass) {
+    this.configureEyeglass(eyeglass, this.sass, details);
+  }
+  details.options = eyeglass.options;
+};
 
 module.exports = EyeglassCompiler;

test/test_eyeglass_plugin.js
@@ -17,14 +17,6 @@ var RSVP = require("rsvp");
 var glob = require("glob");
 var EyeglassCompiler = require("../lib/index");
 
-RSVP.on("error", function(reason, label) {
-  if (label) {
-    console.error(label);
-  }
-
-  console.assert(false, reason.message);
-});
-
 function fixtureSourceDir(name) {
   return path.resolve(__dirname, "fixtures", name, "input");
 }
@@ -36,52 +28,49 @@ function fixtureOutputDir(name) {
 
 function build(builder) {
   return RSVP.Promise.resolve()
-  .then(function() {
-    debugger;
-    return builder.build();
-  })
-  .then(function(hash) {
-    return builder.tree.outputPath;
-  });
+    .then(function() {
+      return builder.build();
+    })
+    .then(function(hash) {
+      return builder.tree.outputPath;
+    });
 }
 
-function diffDirs(actualDir, expectedDir, callback) {
+function assertEqualDirs(actualDir, expectedDir) {
   var actualFiles = glob.sync("**/*", {cwd: actualDir}).sort();
   var expectedFiles = glob.sync("**/*", {cwd: expectedDir}).sort();
+
   assert.deepEqual(actualFiles, expectedFiles);
+
   actualFiles.forEach(function(file) {
     var actualPath = path.join(actualDir, file);
     var expectedPath = path.join(expectedDir, file);
     var stats = fs.statSync(actualPath);
     if (stats.isFile()) {
       assert.equal(fs.readFileSync(actualPath).toString(),
-                   fs.readFileSync(expectedPath).toString());
+        fs.readFileSync(expectedPath).toString());
     }
   });
-  callback();
 }
 
 describe("EyeglassCompiler", function () {
-  it("can be instantiated", function (done) {
+  it("can be instantiated", function () {
     var optimizer = new EyeglassCompiler(fixtureSourceDir("basicProject"), {
       cssDir: "."
     });
     assert(optimizer instanceof EyeglassCompiler);
-    done();
   });
 
-  it("compiles sass files", function (done) {
+  it("compiles sass files", function () {
     var optimizer = new EyeglassCompiler(fixtureSourceDir("basicProject"), {
       cssDir: "."
     });
 
     var builder = new broccoli.Builder(optimizer);
 
-    build(builder)
+    return build(builder)
       .then(function(outputDir) {
-        diffDirs(outputDir, fixtureOutputDir("basicProject"), function() {
-          done();
-        });
+        assertEqualDirs(outputDir, fixtureOutputDir("basicProject"));
       });
   });
 
@@ -131,7 +120,7 @@ describe("EyeglassCompiler", function () {
     );
   });
 
-  it("outputs exceptions when the fullException option is set", function(done) {
+  it("outputs exceptions when the fullException option is set", function() {
     var optimizer = new EyeglassCompiler(fixtureSourceDir("errorProject"), {
       cssDir: ".",
       fullException: true
@@ -141,10 +130,9 @@ describe("EyeglassCompiler", function () {
 
     return build(builder)
       .then(function(outputDir) {
-        diffDirs(outputDir, fixtureOutputDir("basicProject"), done);
+        assertEqualDirs(outputDir, fixtureOutputDir("basicProject"));
       }, function(error) {
         assert.equal("property \"asdf\" must be followed by a ':'", error.message.split("\n")[0]);
-        done();
       });
   });
 

commit c856b46f950c9c41679a1392d8e7d9c68202e851

fix inheritance bug introduced by b07c3ac20e2a4d82498dc18d06a8424d3e55d2cb

lib/broccoli_sass_compiler.js
@@ -159,7 +159,6 @@ function forbidNodeSassOption(options, property) {
 function BroccoliSassCompiler(inputTrees, options) {
   CachingWriter.call(this, inputTrees, options);
 
-  this.name = "BroccoliSassCompiler";
   this.sass = sass;
   this.options = copyObject(options || {});
   this.events = new EventEmitter();
@@ -193,6 +192,7 @@ function BroccoliSassCompiler(inputTrees, options) {
   }
 }
 BroccoliSassCompiler.prototype = Object.create(CachingWriter.prototype);
+BroccoliSassCompiler.prototype.constructor = BroccoliSassCompiler;
 BroccoliSassCompiler.prototype.logCompilationSuccess = function(details, result) {
   var timeInSeconds = result.stats.duration / 1000.0;
   if (timeInSeconds === 0) {

lib/index.js
@@ -45,11 +45,11 @@ function EyeglassCompiler(inputTrees, options) {
 
   // TODO: this should not be accessed before super (ES6 Aligment);
   BroccoliSassCompiler.call(this, inputTrees, options);
-  this.name = "EyeglassCompiler";
   this.events.on("compiling", this.handleNewFile.bind(this));
 }
 
 EyeglassCompiler.prototype = Object.create(BroccoliSassCompiler.prototype);
+EyeglassCompiler.prototype.constructor = EyeglassCompiler;
 EyeglassCompiler.prototype.handleNewFile = function(details) {
   if (!details.options.eyeglass) {
     details.options.eyeglass = {};