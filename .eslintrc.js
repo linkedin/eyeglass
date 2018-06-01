6e128bddf1a6c7521783b337edd52c6364c3f77a
Robert Jackson <rjackson@linkedin.com>
2018-06-01 09:18:04 -0400

    Update to ember-cli@3.1.4 blueprint.

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,10 +4,47 @@ module.exports = {
     ecmaVersion: 2017,
     sourceType: 'module'
   },
-  extends: 'eslint:recommended',
+  plugins: [
+    'ember'
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:ember/recommended'
+  ],
   env: {
     browser: true
   },
   rules: {
-  }
+  },
+  overrides: [
+    // node files
+    {
+      files: [
+        'ember-cli-build.js',
+        'index.js',
+        'testem.js',
+        'blueprints/*/index.js',
+        'config/**/*.js',
+        'tests/dummy/config/**/*.js'
+      ],
+      excludedFiles: [
+        'addon/**',
+        'addon-test-support/**',
+        'app/**',
+        'tests/dummy/app/**'
+      ],
+      parserOptions: {
+        sourceType: 'script',
+        ecmaVersion: 2015
+      },
+      env: {
+        browser: false,
+        node: true
+      },
+      plugins: ['node'],
+      rules: Object.assign({}, require('eslint-plugin-node').configs.recommended.rules, {
+        // add your custom rules and overrides for node files here
+      })
+    }
+  ]
 };
--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-/* eslint-env node */
 'use strict';
 
 const EyeglassCompiler = require('broccoli-eyeglass');
