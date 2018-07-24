c5b7a0a04a17f492092bfdf856e27245e81639be
Chad Hietala <chadhietala@gmail.com>
2018-07-24 15:49:45 -0400

    Lookup paths differ between eager and lazy

--- tests/dummy/lib/eager/index.js
+++ tests/dummy/lib/eager/index.js
@@ -2,12 +2,20 @@
 
 // eslint-disable-next-line node/no-extraneous-require
 const EngineAddon = require('ember-engines/lib/engine-addon');
+const path = require('path');
+const assetsDir = path.join(__dirname, '/public/assets');
 
 module.exports = EngineAddon.extend({
   name: 'eager',
 
   lazyLoading: Object.freeze({
     enabled: false
+  }),
+
+
+  eyeglass: Object.freeze({
+    discover: true,
+    assets: assetsDir,
   }),
 
   isDevelopingAddon() {
