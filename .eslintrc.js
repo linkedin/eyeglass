fa895f06cb5ba576235b1001fa5296b37470290e
Gabriel J. Csapo <gcsapo@linkedin.biz>
2018-11-02 17:49:15 -0700

    [dependencies] bump dependencies, use eslint, mocha and nyc directly

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  parserOptions: {
+    ecmaVersion: 2017,
+    sourceType: 'module'
+  },
+  extends: [
+    'eslint:recommended',
+  ],
+  env: {
+    es6: true,
+    node: true,
+    mocha: true
+  }
+};
