f9132636164144049b6facc1413a7a02909790be
Chris Eppstein <chris@eppsteins.net>
2019-02-07 14:27:46 -0800

    Simplify definition of isPresent.

--- packages/eyeglass/src/util/typescriptUtils.ts
+++ packages/eyeglass/src/util/typescriptUtils.ts
@@ -2,6 +2,6 @@ export interface Dict<T> {
   [key: string]: T;
 }
 
-export function isPresent<T>(v: T): v is Exclude<T, undefined | null> {
+export function isPresent<T>(v: T | undefined | null): v is T {
   return typeof v !== "undefined" && v !== null
 }
